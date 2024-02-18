9312c8c9681e14204210a6e84b6a2d53f20af038
Nino Akut <akutnino@gmail.com>
2024-02-18 23:12:57 +0800

    recreated the component with jsx syntax

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 function App() {
-  return <h1>This is a React Component</h1>;
+  const sampleText = 'and it is awesome';
+
+  return (
+    <div>
+      <h1>This is a React Component {sampleText}</h1>
+      <p>This is a sample paragraph element</p>
+      <button type="button">Click Me</button>
+      <input id="textInput" type="text" title="text-input"></input>
+    </div>
+  );
 }
 
 export default App;
