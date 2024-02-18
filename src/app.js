5f319ee7e7c315fd34796288a88f81d89037cd1c
Nino Akut <akutnino@gmail.com>
2024-02-18 23:35:38 +0800

    added styling to the component with scss

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
+import './app.css';
+
 function App() {
   const sampleText = 'and it is awesome';
 
   return (
-    <div>
-      <h1>This is a React Component {sampleText}</h1>
-      <p>This is a sample paragraph element</p>
-      <button type="button">Click Me</button>
+    <div id="appMainDiv">
+      <h1 id="main-h1">This is a React Component {sampleText}</h1>
+      <p id="main-p">This is a sample paragraph element</p>
+      <button className="btn" type="button">
+        Click Me
+      </button>
       <input id="textInput" type="text" title="text-input"></input>
     </div>
   );
