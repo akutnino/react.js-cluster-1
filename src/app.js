0c16a4d038531553eb51b828cd288388042ae460
Nino Akut <akutnino@gmail.com>
2024-02-18 22:35:02 +0800

    tried creating a component with pure javascript

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 function App() {
-  return <h1>This is a React Component</h1>;
+  const sampleText = 'This is a sample text.';
+  const paragraphElement = document.createElement('p');
+  const paragraphTextNode = document.createTextNode(sampleText);
+  paragraphElement.appendChild(paragraphTextNode);
+  paragraphElement.id = 'main-paragraph';
+  paragraphElement.className = 'mainElement';
+
+  return paragraphElement;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
 
-const root = ReactDOM.createRoot(document.querySelector('#root'));
-root.render(<App></App>);
+// const root = ReactDOM.createRoot(document.querySelector('#root'));
+// root.render(<App></App>);
+
+const root = document.querySelector('#root');
+root.appendChild(App());
