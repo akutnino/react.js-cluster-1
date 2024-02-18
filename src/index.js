f724f21f36da5ec8261f83f19b70d437562014b9
Nino Akut <akutnino@gmail.com>
2024-02-18 22:04:03 +0800

    deleted reactDom.render() since it is depreciated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const header = <h1>This is my first react app</h1>;
-ReactDOM.render(header, document.querySelector('#root'));
-
-console.log(header);
