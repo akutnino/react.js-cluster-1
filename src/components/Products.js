1df5740567f513b2de45c4afa90f430842d8965b
Nino Akut <akutnino@gmail.com>
2024-02-19 22:16:27 +0800

    working with jsx attributes : require() method and style = {} attribute

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,9 @@
 import './Products.css';
 
-let productCount = 100;
+const productCount = 100;
+const imageUrl = require('./../images/logo192.png');
+const imageStyle = { width: '50px', height: '50px' };
+// const localProductStyles = { 'font-size': '25px' };
 
 function displayProductCount() {
   return productCount > 0 ? productCount : 'Zero';
@@ -11,11 +14,18 @@ function Products() {
     <div id="products-div">
       <h1 id="products-h1">All Available Products</h1>
       <ul id="priducts-ul">
-        <li className="products-item">List Item One</li>
-        <li className="products-item">List Item Two</li>
-        <li className="products-item">List Item Three</li>
+        <li className="products-item" style={{ fontSize: '25px' }}>
+          List Item One
+        </li>
+        <li className="products-item" style={{ fontSize: '25px' }}>
+          List Item Two
+        </li>
+        <li className="products-item" style={{ fontSize: '25px' }}>
+          List Item Three
+        </li>
       </ul>
       <div>
+        <img src={imageUrl} alt="react-logo" style={imageStyle} />
         <button type="button">-</button>
         <label id="product-label"> {displayProductCount()} </label>
         <button type="button">+</button>
