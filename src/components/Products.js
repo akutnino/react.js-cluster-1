59f641cc00fdb827ffbe9e13b28edcb05b0fcdbf
Nino Akut <akutnino@gmail.com>
2024-02-20 00:57:53 +0800

    revamped the Products.js ui to align with the course

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,11 +2,16 @@ import './Products.css';
 
 const productCount = 100;
 const imageUrl = require('./../images/logo192.png');
-const imageStyle = { width: '50px', height: '50px' };
-// const localProductStyles = { 'font-size': '25px' };
-const isAvailable = 'Un-Available';
+const basicStyle = {
+  margin: '10px',
+  padding: '5px',
+  border: '1px solid black',
+};
+const isAvailable = 'Available';
 const productAvailability =
   isAvailable === 'Available' ? 'Available' : 'Un-Available';
+const productbadgeClass =
+  isAvailable === 'Available' ? 'badge bg-success' : 'badge bg-danger';
 
 function displayProductCount() {
   return productCount > 0 ? productCount : 'Zero';
@@ -15,25 +20,36 @@ function displayProductCount() {
 function Products() {
   return (
     <div id="products-div">
-      <h1 id="products-h1">All Available Products</h1>
-      <ul id="priducts-ul">
-        <li className="products-item" style={{ fontSize: '25px' }}>
-          List Item One
-        </li>
-        <li className="products-item" style={{ fontSize: '25px' }}>
-          List Item Two
-        </li>
-        <li className="products-item" style={{ fontSize: '25px' }}>
-          List Item Three
+      <ul className="list-group shadow">
+        <li className="list-group-item">
+          <div className="li-div">
+            <div className="product-div-child">
+              <h5 className="mt-0 font-weight-bold mb-2">Fresh Milk</h5>
+              <p className="font-italic text-muted mb-0 small">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Suscipit fuga autem maiores necessitatibus.
+              </p>
+              <div className="d-flex align-items-center justify-content-start mt-1">
+                <h6 className="font-weight-bold my-2">$120.00</h6>
+                <button className="btn btn-primary">-</button>
+                <label className="mylabel" style={basicStyle}>
+                  {displayProductCount()}
+                </label>
+                <button className="btn btn-primary">+</button>
+                <label className={productbadgeClass} style={basicStyle}>
+                  {productAvailability}
+                </label>
+              </div>
+            </div>
+            <img
+              src={imageUrl}
+              alt="Generic Placeholder"
+              width="200"
+              className="product-image"
+            />
+          </div>
         </li>
       </ul>
-      <div>
-        <img src={imageUrl} alt="react-logo" style={imageStyle} />
-        <button type="button">-</button>
-        <label id="product-label"> {displayProductCount()} </label>
-        <button type="button">+</button>
-        <h3 className={productAvailability}>This Product is: {isAvailable} </h3>
-      </div>
     </div>
   );
 }
