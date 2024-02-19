258640da2e724876eafaea545f0d4344f00bb4e4
Nino Akut <akutnino@gmail.com>
2024-02-20 02:06:59 +0800

    passing props data via applying jsx attributes to the Products component

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,36 +1,35 @@
 import './Products.css';
 
 const productCount = 100;
-const imageUrl = require('./../images/logo192.png');
+// const imageUrl = require('./../images/logo192.png');
 const basicStyle = {
   margin: '10px',
   padding: '5px',
   border: '1px solid black',
 };
-const isAvailable = 'Available';
-const productAvailability =
-  isAvailable === 'Available' ? 'Available' : 'Un-Available';
-const productbadgeClass =
-  isAvailable === 'Available' ? 'badge bg-success' : 'badge bg-danger';
 
 function displayProductCount() {
   return productCount > 0 ? productCount : 'Zero';
 }
 
-function Products() {
+function Products(props = {}) {
+  const productAvailability =
+    props.isAvailable === true ? 'Available' : 'Un-Available';
+  const productbadgeClass =
+    props.isAvailable === true ? 'badge bg-success' : 'badge bg-danger';
+
   return (
     <div id="products-div">
       <ul className="list-group shadow">
         <li className="list-group-item">
           <div className="li-div">
             <div className="product-div-child">
-              <h5 className="mt-0 font-weight-bold mb-2">Fresh Milk</h5>
+              <h5 className="mt-0 font-weight-bold mb-2">{props.name}</h5>
               <p className="font-italic text-muted mb-0 small">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Suscipit fuga autem maiores necessitatibus.
+                {props.description}
               </p>
               <div className="d-flex align-items-center justify-content-start mt-1">
-                <h6 className="font-weight-bold my-2">$120.00</h6>
+                <h6 className="font-weight-bold my-2">${props.price}</h6>
                 <button className="btn btn-primary">-</button>
                 <label className="mylabel" style={basicStyle}>
                   {displayProductCount()}
@@ -42,7 +41,7 @@ function Products() {
               </div>
             </div>
             <img
-              src={imageUrl}
+              src={props.imageUrl}
               alt="Generic Placeholder"
               width="200"
               className="product-image"
