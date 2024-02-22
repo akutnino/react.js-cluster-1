a759398b441185dcd4881c63f347cfc7321c3c76
Nino Akut <akutnino@gmail.com>
2024-02-23 02:10:04 +0800

    moved the products array to App.js, and implemented a Stateful List

--- src/app.js
+++ src/app.js
@@ -3,17 +3,59 @@ import './App.css';
 import ProductList from './components/ProductList/ProductList';
 import CreateProduct from './components/CreateProduct/CreateProduct';
 
-function App() {
-  let [newProduct, updateProduct] = useState(null);
+const products = [
+  {
+    pID: 1,
+    pName: 'Fresh Milk',
+    desc: ' Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    isAvailable: true,
+    image: 'images/fresh-milk.png',
+    price: 14,
+  },
+  {
+    pID: 2,
+    pName: 'Cottage Cheese',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    isAvailable: false,
+    image: 'images/cottage-cheese.png',
+    price: 10,
+  },
+  {
+    pID: 3,
+    pName: 'Brocoli',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    isAvailable: true,
+    image: 'images/brocoli.png',
+    price: 15,
+  },
+  {
+    pID: 4,
+    pName: 'oranges',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    isAvailable: true,
+    image: 'images/oranges.png',
+    price: 20,
+  },
+  {
+    pID: 5,
+    pName: 'Olive oil',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    isAvailable: false,
+    image: 'images/olive-oil.png',
+    price: 14,
+  },
+];
 
+function App() {
+  let [productList, updateProductList] = useState(products);
   const createProduct = function (product) {
-    updateProduct(product);
+    updateProductList((previousList) => [product, ...previousList]);
   };
 
   return (
     <div>
       <CreateProduct createProduct={createProduct}></CreateProduct>
-      <ProductList newProduct={newProduct}></ProductList>
+      <ProductList productList={productList}></ProductList>
     </div>
   );
 }
--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,69 +1,26 @@
 import React from 'react';
 import Products from './Products';
 
-const products = [
-  {
-    pID: 1,
-    pName: 'Fresh Milk',
-    desc: ' Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    isAvailable: true,
-    image: 'images/fresh-milk.png',
-    price: 14,
-  },
-  {
-    pID: 2,
-    pName: 'Cottage Cheese',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-    isAvailable: false,
-    image: 'images/cottage-cheese.png',
-    price: 10,
-  },
-  {
-    pID: 3,
-    pName: 'Brocoli',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-    isAvailable: true,
-    image: 'images/brocoli.png',
-    price: 15,
-  },
-  {
-    pID: 4,
-    pName: 'oranges',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-    isAvailable: true,
-    image: 'images/oranges.png',
-    price: 20,
-  },
-  {
-    pID: 5,
-    pName: 'Olive oil',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-    isAvailable: false,
-    image: 'images/olive-oil.png',
-    price: 14,
-  },
-];
-
 const ProductList = (props = {}) => {
   const sampleText = 'and it is awesome';
-  console.log(props.newProduct);
+  const productListArray = props.productList.map((product) => {
+    return (
+      <Products
+        id={product.pID}
+        name={product.pName}
+        description={product.desc}
+        isAvailable={product.isAvailable}
+        imageUrl={product.image}
+        price={product.price}
+      ></Products>
+    );
+  });
 
   return (
     <div id="appMainDiv">
       <h1 id="main-h1">This is a React Component {sampleText}</h1>
       <p id="main-p">This is a sample paragraph element</p>
-      {products.map((product) => {
-        return (
-          <Products
-            id={product.pID}
-            name={product.pName}
-            description={product.desc}
-            isAvailable={product.isAvailable}
-            imageUrl={product.image}
-            price={product.price}
-          ></Products>
-        );
-      })}
+      {productListArray}
       <button className="btn" type="button">
         Click Me
       </button>
